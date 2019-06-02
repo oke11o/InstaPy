9d60bdf8ce725002d53148edcfadda23cdae073c
Sergey Bevzenko <sbevzenko@ozon.ru>
2019-06-02 19:18:45 +0300

    WIP

--- instapy/__init__.py
+++ instapy/__init__.py
@@ -2,6 +2,9 @@
 
 from .instapy import InstaPy
 from .util import smart_run
+from .util import click_element
+from .util import web_address_navigator
+from .time_util import sleep
 from .settings import Settings
 from .file_manager import set_workspace
 from .file_manager import get_workspace
---
+++ oke11o.py
@@ -0,0 +1,114 @@
+from instapy import InstaPy
+from instapy import smart_run
+from instapy import click_element
+from instapy import sleep
+from instapy import web_address_navigator
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import StaleElementReferenceException
+from selenium.common.exceptions import WebDriverException
+import random
+
+
+def watch_the_story(browser, logger):
+
+    user_link = "https://www.instagram.com/stories/kirill.toros/"
+    # check URL of the webpage, if it already is user's profile page,
+    # then do not navigate to it again
+    web_address_navigator(browser, user_link)
+
+    # xpaths
+    prev_xpath = "//button/div[@class='coreSpriteRightChevron']"
+    next_xpath = "//button/div[@class='coreSpriteRightChevron']"
+    close_xpath = "//button/div[@class='coreSpriteCloseLight']"
+    prev_elem = None
+    next_elem = None
+    try:
+        # check if there is a story for this user according to IG user data
+        is_story = browser.execute_script(
+            "return window._sharedData.entry_data.StoriesPage[0].user.id")
+        print(is_story)
+        is_story = True
+        if is_story:
+            # first img is a story
+            first_imag = browser.find_element_by_tag_name("img")
+            click_element(browser, first_imag)
+            sleep(2)
+            # find the buttons before story ends to save time on xpath fail
+            close_elem = browser.find_element_by_xpath(close_xpath)
+            while random.randint(1,5) != 1:
+                # find the element
+                if next_elem is None:
+                    next_elem = browser.find_element_by_xpath(next_xpath)
+                sleep(random.randint(2,6))
+                # move to the right (next)
+                click_element(browser, next_elem)
+                sleep(2) # let the prev button load
+                if random.randint(1, 10) == 1:
+                    if prev_elem is None:
+                        prev_elem = browser.find_element_by_xpath(prev_xpath)
+                    sleep(random.randint(2, 4))
+                    # play with it, we can go back
+                    click_element(browser, prev_elem)
+            sleep(5)
+            # close button since this is a long story..
+            click_element(browser, close_elem)
+    except NoSuchElementException:
+        # story is finished
+        pass
+    except StaleElementReferenceException:
+        # story is finished
+        pass
+    except WebDriverException:
+        logger.error('has_highlight_reel not exist')
+    except:
+        logger.error('Story: something went wrong')
+
+insta_username = ''
+insta_password = ''
+
+# get an InstaPy session!
+session = InstaPy(username=insta_username,
+                  password=insta_password,
+                  headless_browser=False)
+
+with smart_run(session):
+    """ Activity flow """
+    # general settings
+    # session.set_dont_include(["friend1", "friend2", "friend3"])
+
+    session.set_relationship_bounds(enabled=True,
+                                    potency_ratio=-1.21,
+                                    delimit_by_numbers=True,
+                                    max_followers=4590,
+                                    max_following=5555,
+                                    min_followers=45,
+                                    min_following=77)
+
+    session.set_do_comment(True, percentage=10)
+    session.set_comments(['amazing!', 'So much fun!!', 'Nicey!', 'Cool!!', 'Awesome!'])
+
+
+    watch_the_story(session.browser, session.logger)
+    # activity
+    # session.like_by_tags([
+    #     'sport',
+    #     # 'russia',
+    #     # 'programming',
+    #     # 'developer',
+    #     # 'travel',
+    #     # 'trip',
+    #     # 'it',
+    #     # 'symfony',
+    #     # 'travel',
+    #     # 'trip',
+    #     # 'relax',
+    #     # 'rest',
+    #     # 'happy',
+    #     # 'счастье',
+    #     # 'москва',
+    #     # 'программирование',
+    #     # 'отдых'
+    # ], amount=2)
+    print("DONE")
+
+
